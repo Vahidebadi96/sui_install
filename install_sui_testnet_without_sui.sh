544d8acb3d2d5c67271f85f9cfd7cafe7a76e803
vahidebadi96 <ebadivahid996@gmail.com>
2023-04-27 06:29:36 +0330

    first

--- install_sui_testnet_without_sui.sh
+++ install_sui_testnet_without_sui.sh
@@ -52,8 +52,10 @@ echo "Enter wallet address:"
 
 read walletaddress
 
-switch client --address $walletaddress
+sui client switch --address $walletaddress
 
 sui client publish --gas-budget 30000000
 
-echo "Import your wallet ,Dont forget to check your current wallet address. run sui switch --address 'your address'"
+
+
+sui client call --function sword_create --module my_module --package 0xc4332ab7a0bbfde793f202d6a8c93e1b436bc9a54c8b71265245f3265eb649be --args 0x2b4dfdbd42bb7ddf993b89425c68e9fb27cccef6d76805ce8f9cf64051ad810a 42 7 0x5b9ae824a03001c076e8c9055eb5297908465583b3a5c0e06097cdf1f71648c5 --gas-budget 30000000
